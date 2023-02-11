ae2bb29738ef56ed0a345c70d49087485227ecb7
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-11 21:09:41 +0100

    Init

--- uninstall.sh
+++ uninstall.sh
@@ -6,40 +6,40 @@ then
 	exit 1
 fi
 
-systemctl stop display_rotation_mode_switcher
+systemctl stop display_orientation_mode_switcher
 if [[ $? != 0 ]]
 then
-	echo "display_rotation_mode_switcher.service cannot be stopped correctly..."
+	echo "display_orientation_mode_switcher.service cannot be stopped correctly..."
 	exit 1
 fi
 
-systemctl disable display_rotation_mode_switcher
+systemctl disable display_orientation_mode_switcher
 if [[ $? != 0 ]]
 then
-	echo "display_rotation_mode_switcher.service cannot be disabled correctly..."
+	echo "display_orientation_mode_switcher.service cannot be disabled correctly..."
 	exit 1
 fi
 
-rm -f /lib/systemd/system/display_rotation_mode_switcher.service
+rm -f /lib/systemd/system/display_orientation_mode_switcher.service
 if [[ $? != 0 ]]
 then
-	echo "/lib/systemd/system/display_rotation_mode_switcher.service cannot be removed correctly..."
+	echo "/lib/systemd/system/display_orientation_mode_switcher.service cannot be removed correctly..."
 	exit 1
 fi
 
-rm -rf /usr/share/display_rotation_mode_switcher-driver/
+rm -rf /usr/share/display_orientation_mode_switcher-driver/
 if [[ $? != 0 ]]
 then
-	echo "/usr/share/display_rotation_mode_switcher-driver/ cannot be removed correctly..."
+	echo "/usr/share/display_orientation_mode_switcher-driver/ cannot be removed correctly..."
 	exit 1
 fi
 
-rm -rf /var/log/display_rotation_mode_switcher-driver
+rm -rf /var/log/display_orientation_mode_switcher-driver
 if [[ $? != 0 ]]
 then
-	echo "/var/log/display_rotation_mode_switcher-driver cannot be removed correctly..."
+	echo "/var/log/display_orientation_mode_switcher-driver cannot be removed correctly..."
 	exit 1
 fi
 
-echo "Display rotation mode switcher python driver uninstalled"
+echo "Display orientation mode switcher python driver uninstalled"
 exit 0
