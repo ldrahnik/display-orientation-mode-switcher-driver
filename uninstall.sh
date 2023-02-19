c9e5e680222cedcf7f2c6079995d2403e4ba4b1f
Lukáš Drahník <ldrahnik@gmail.com>
2023-02-19 12:56:47 +0100

    To un-install script added possibility do not remove conf files when differ from default ones

--- uninstall.sh
+++ uninstall.sh
@@ -27,11 +27,43 @@ then
 	exit 1
 fi
 
-rm -rf /usr/share/display_orientation_mode_switcher-driver/
-if [[ $? != 0 ]]
+CONF_DIR="/usr/share/display_orientation_mode_switcher-driver/conf/"
+
+CONF_DIR_DIFF=""
+if test -d "$CONF_DIR"; then
+    CONF_DIR_DIFF=$(diff conf $CONF_DIR)
+fi
+
+if [ "$CONF_DIR_DIFF" != "" ]
 then
-	echo "/usr/share/display_orientation_mode_switcher-driver/ cannot be removed correctly..."
-	exit 1
+    read -r -p "Installed config scripts contain modifications compared to the default ones. Do you want remove them for each mode [y/N]" response
+    case "$response" in [yY][eE][sS]|[yY])
+		rm -rf /usr/share/display_orientation_mode_switcher-driver/
+		if [[ $? != 0 ]]
+		then
+			echo "/usr/share/display_orientation_mode_switcher-driver/ cannot be removed correctly..."
+			exit 1
+		fi
+        ;;
+    *)
+		shopt -s extglob
+		rm -rf /usr/share/display_orientation_mode_switcher-driver/!(conf)
+		if [[ $? != 0 ]]
+		then
+			echo "/usr/share/display_orientation_mode_switcher-driver/ cannot be removed correctly..."
+			exit 1
+		fi
+		echo "Config scripts in /usr/share/power_supply_mode_switcher-driver/conf/ have not been removed and remain in system:"
+        ls /usr/share/display_orientation_mode_switcher-driver/conf
+        ;;
+    esac
+else
+	rm -rf /usr/share/display_orientation_mode_switcher-driver/
+	if [[ $? != 0 ]]
+	then
+		echo "/usr/share/display_orientation_mode_switcher-driver/ cannot be removed correctly..."
+		exit 1
+	fi
 fi
 
 rm -rf /var/log/display_orientation_mode_switcher-driver
